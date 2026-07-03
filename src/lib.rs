a02b7801145aa9c483e7f2f5568b23b2d95fd655
Ryn Easterbrook <rynsterbrook@gmail.com>
2026-07-03 11:51:56 +0100

    Fix lib.rs modules for optional bevy features

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,19 @@
+#[cfg(feature = "bevy")]
 mod gizmos;
 mod grid;
 mod region;
+#[cfg(feature = "bevy")]
 mod relations;
-#[cfg(feature = "bevy")] mod square;
-#[cfg(feature = "bevy")] mod tilemap;
+mod square;
+mod tilemap;
 
 pub mod prelude {
+    #[cfg(feature = "bevy")]
+    pub use crate::gizmos::{GridGizmo, GridGizmoPlugin};
     pub use crate::grid::{Grid, GridGeometry, GridTopology, PointQuery};
     pub use crate::region::{RectRegion, Region};
+    #[cfg(feature = "bevy")]
+    pub use crate::relations::{TilemapOf, Tilemaps};
     pub use crate::square::SquareGrid;
-    pub use crate::tilemap::Tilemap;                              // core
-    #[cfg(feature = "bevy")] pub use crate::relations::{TilemapOf, Tilemaps};
-    #[cfg(feature = "bevy")] pub use crate::gizmos::{GridGizmo, GridGizmoPlugin};
+    pub use crate::tilemap::Tilemap;
 }
